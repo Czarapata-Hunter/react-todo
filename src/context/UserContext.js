97b8bfd0d225df034e8e43510809c62d90e9912b
Hunter Czarapata <czarapata.hunter@gmail.com>
2023-01-30 12:57:05 -0800

    UserContext created and exporting out

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,1 +1,22 @@
+const { createContext, useState, useContext } = require('react');
+const { getUser } = require('../services/auth');
 
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
+
+const useUser = () => {
+  const data = useContext(UserContext);
+
+  if (!data) {
+    throw new Error('useUser must be encompassed in a UserProvider');
+  }
+  return data;
+};
+
+export { UserProvider, useUser };
--- src/services/auth.js
+++ src/services/auth.js
@@ -3,16 +3,3 @@ import { client } from '/client';
 export function getUser() {
   return client.auth.currentUser;
 }
-
-export async function authUser(email, password, type) {
-  let response;
-  if (type === 'sign-up') {
-    response = await client.auth.signUp({ email, password });
-  } else {
-    response = await client.auth.signIn({ email, password });
-  }
-  if (response.error) {
-    throw response.error;
-  }
-  return response.error;
-}
