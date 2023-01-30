5c1d3e56668bcbba366dbe9c6e4f662e632fad7c
Hunter Czarapata <czarapata.hunter@gmail.com>
2023-01-30 15:17:29 -0800

    Removed placeholder auth and added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Auth from './components/Auth/Auth.js';
+import Header from './components/Header/Header.js';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Auth />
+      <Header />
+      <Switch>
+        <Route path="/auth/:type" component={Auth} />
+        <Route exact path="/" />
+      </Switch>
+      {/* <Auth /> */}
     </div>
   );
 }
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,4 @@
-import { client } from '/client';
+import { client } from '../services/client.js';
 
 export function getUser() {
   return client.auth.currentUser;
